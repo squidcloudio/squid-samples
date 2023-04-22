a826a305bc5febc484cd02987739f38386b21ca1
Giorgi <Giorgi.Mrelashvili@itechart-group.com>
2023-04-23 01:04:40 +0400

    fix: update colors list & tags

--- todo-react/src/constants/colors.ts
+++ todo-react/src/constants/colors.ts
@@ -5,6 +5,7 @@ const colors = [
   { color: '#78FF8E', title: 'Green' },
   { color: '#42FEFE', title: 'Aqua' },
   { color: '#4484E5', title: 'Blue' },
+  { color: '#9747FF', title: 'Purple' },
 ];
 
 export default colors;
