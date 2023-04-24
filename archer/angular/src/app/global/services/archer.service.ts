751f684b0755ae39b338ecd643316cab396d73db
Nir Peled <peledni@gmail.com>
2023-04-24 14:09:45 -0400

    Switched to using 'or' from the client

--- archer/angular/src/app/global/services/archer.service.ts
+++ archer/angular/src/app/global/services/archer.service.ts
@@ -1,25 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CollectionReference, Squid } from '@squidcloud/client';
 import { ArcherUser, PortfolioValueHistory, Ticker, UserAsset } from 'archer-common';
-import { combineLatest, firstValueFrom, map, Observable, share } from 'rxjs';
-import { DocumentReference } from '@squidcloud/client/dist/typescript-client/src/document-reference';
-import { DocumentData } from '@squidcloud/common';
+import { map, Observable, share } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArcherService {
-  // TODO: check if need to add a 'share()' pipe
-  private readonly tickersObs = this.getTickerCollection()
-    .query()
-    .limit(20000)
-    .sortBy('id')
-    .snapshots()
-    .pipe(
-      map((snapshots) => snapshots.map((snapshot) => snapshot.data)),
-      share(),
-    );
-
   private readonly assetsObs = this.getUserAssetCollection()
     .query()
     .sortBy('tickerId')
@@ -31,27 +18,18 @@ export class ArcherService {
 
   constructor(private readonly squid: Squid) {}
 
-  observeTickers(): Observable<Ticker[]> {
-    return this.tickersObs;
-  }
-
   observeAssets(): Observable<UserAsset[]> {
     return this.assetsObs;
   }
 
-  searchTickers(query: string): Promise<Ticker[]> {
-    return firstValueFrom(
-      or<Ticker>(
-        [
-          {
-            fieldName: 'id',
-            asc: true,
-          },
-        ],
-        this.getTickerCollection().query().like('id', `%${query}%`, false).limit(100).sortBy('id').snapshots(false),
-        this.getTickerCollection().query().like('name', `%${query}%`, false).limit(100).sortBy('id').snapshots(false),
-      ).pipe(map((snapshots) => snapshots.map((snapshot) => snapshot.data))),
-    );
+  async searchTickers(query: string): Promise<Ticker[]> {
+    const results = await this.getTickerCollection()
+      .or(
+        this.getTickerCollection().query().like('id', `%${query}%`, false).limit(100).sortBy('id'),
+        this.getTickerCollection().query().like('name', `%${query}%`, false).limit(100).sortBy('id'),
+      )
+      .snapshot();
+    return results.map((result) => result.data);
   }
 
   private getTickerCollection(): CollectionReference<Ticker> {
@@ -69,42 +47,4 @@ export class ArcherService {
   private getPortfolioValueHistoryCollection(): CollectionReference<PortfolioValueHistory> {
     return this.squid.collection<PortfolioValueHistory>('portfolioValueHistory');
   }
-}
-
-type SortOrder = {
-  fieldName: string;
-  asc: boolean;
-};
-
-function or<T extends DocumentData>(
-  sort: Array<SortOrder>,
-  ...observables: Observable<Array<DocumentReference<T>>>[]
-): Observable<Array<DocumentReference<T>>> {
-  return combineLatest([...observables]).pipe(
-    map((results) => {
-      const seenData = new Set<T>();
-      const flatResult = results.flat();
-      const result: Array<DocumentReference<T>> = [];
-      for (const docRef of flatResult) {
-        if (seenData.has(docRef.data)) {
-          continue;
-        }
-        seenData.add(docRef.data);
-        result.push(docRef);
-      }
-      return result.sort((a, b) => {
-        for (const { fieldName, asc } of sort) {
-          const aVal = a.data[fieldName];
-          const bVal = b.data[fieldName];
-          if (aVal < bVal) {
-            return asc ? -1 : 1;
-          }
-          if (aVal > bVal) {
-            return asc ? 1 : -1;
-          }
-        }
-        return 0;
-      });
-    }),
-  );
 }
