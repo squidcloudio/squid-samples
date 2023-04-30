e58de3e378e162e5466caf9202b8d5f5119af86a
Nir Peled <peledni@gmail.com>
2023-04-29 22:05:34 -0400

    Removed unneeded code

--- archer/angular/src/app/global/components/protected-layout/protected-layout.component.ts
+++ archer/angular/src/app/global/components/protected-layout/protected-layout.component.ts
@@ -1,8 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ArcherService } from '../../services/archer.service';
-import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
-import { BehaviorSubject, debounce, filter, interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-protected-layout',
@@ -13,7 +10,6 @@ import { BehaviorSubject, debounce, filter, interval, switchMap } from 'rxjs';
 export class ProtectedLayoutComponent {
   searchBarVisible = false;
   userObs = this.archerService.observeUser();
-  userAssetsObs = this.archerService.observeUserAssets();
 
   constructor(private readonly archerService: ArcherService) {}
 
