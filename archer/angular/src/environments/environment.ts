52606edeb3fbf8bc16103e732e3203a63d474e54
Nir Peled <peledni@gmail.com>
2023-06-15 14:03:11 -0400

    Updated archer with the new local backend structure

--- archer/angular/src/environments/environment.ts
+++ archer/angular/src/environments/environment.ts
@@ -1,7 +1,9 @@
 import { EnvironmentType } from './environment.types';
 
 export const environment: EnvironmentType = {
-  squidRegion: 'local',
+  squidRegion: 'us-east-1.aws',
+  squidDeveloperId: 'nir1',
+  environmentId: 'dev',
   squidAppId: 'archer',
   auth0ClientId: 'aF3jhIcL7YnNFl90VZeN9JB5PzLcOSAV',
   auth0Domain: 'squid-archer.us.auth0.com',
