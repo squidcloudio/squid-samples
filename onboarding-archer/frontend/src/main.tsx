7f642ab13a38b90cd92bfd9cf0ca0a8d433cf5fd
Nir Peled <peledni@gmail.com>
2023-08-01 14:52:51 -0400

    Created a script that prepares the schematics directory

--- onboarding-archer/frontend/src/main.tsx
+++ onboarding-archer/frontend/src/main.tsx
@@ -7,10 +7,10 @@ import { ArcherContextProvider } from '@/utils/ArcherContextProvider.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <SquidContextProvider
     options={{
-      appId: 'm46jhnqhfzv8ujey2d',
-      region: 'us-east-1.aws',
-      environmentId: 'dev',
-      squidDeveloperId: 'nir2',
+      appId: import.meta.env.VITE_APP_ID,
+      region: import.meta.env.VITE_REGION,
+      environmentId: import.meta.env.VITE_ENVIRONMENT_ID,
+      squidDeveloperId: import.meta.env.VITE_SQUID_DEVELOPER_ID,
     }}
   >
     <ArcherContextProvider>
