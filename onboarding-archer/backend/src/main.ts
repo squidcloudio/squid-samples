8439a0ec349b73b8ff8d2f158c0c804ccb3a8b10
Nir Peled <peledni@gmail.com>
2023-08-05 16:54:50 -0400

    Doc changes

--- onboarding-archer/backend/src/main.ts
+++ onboarding-archer/backend/src/main.ts
@@ -5,6 +5,7 @@ import * as backendService from './index';
  *                                     *
  *    SQUID CLOUD INTERNAL USE ONLY    *
  *    PLEASE DO NOT MODIFY             *
+ *    THIS FILE IS USED FOR LOCAL DEV  *
  *                                     *
  **************************************/
 
