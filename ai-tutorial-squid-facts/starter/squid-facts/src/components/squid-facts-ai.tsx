762a06bd08da5b84fcaf43a0622cac52095ccff2
Victor Chang <victor@squid.cloud>
2023-11-15 10:57:22 -0800

    Explicitly fail on placeholder for `ai-tutorial-squid-facts`

--- ai-tutorial-squid-facts/starter/squid-facts/src/components/squid-facts-ai.tsx
+++ ai-tutorial-squid-facts/starter/squid-facts/src/components/squid-facts-ai.tsx
@@ -1,8 +1,3 @@
-import { ChangeEvent, useState } from 'react';
-import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
-import Messages from './messages';
-
 function SquidFactsAI() {
   return <></>;
 }
