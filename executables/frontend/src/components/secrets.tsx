c7c523f355b420afb23a29ccfb898baab2585054
Victor Chang <victor@squid.cloud>
2023-11-27 17:24:07 -0800

    Follow the Figma design mostly.

--- executables/frontend/src/components/secrets.tsx
+++ executables/frontend/src/components/secrets.tsx
@@ -1,25 +1,85 @@
 import { useSquid } from '@squidcloud/react';
-import { Button } from '@mui/material';
+import { IconButton } from '@mui/material';
+import CelebrationIcon from '@mui/icons-material/Celebration';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ErrorIcon from '@mui/icons-material/Error';
+import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import React, { useState } from 'react';
 
-interface StatusProps {
+interface ApiKeyProps {
   text: string;
 }
 
-const StatusComponent: React.FC<StatusProps> = ({ text }) => {
-  return <p>{text}</p>;
+enum StatusStates {
+  Idle = 'IDLE',
+  Loading = 'LOADING',
+  Valid = 'VALID',
+  Invalid = 'INVALID',
+}
+
+interface StatusProps {
+  status: StatusStates;
+}
+
+function obfuscateText(
+  text: string,
+  visibleStart = 5,
+  hiddenLen = 16,
+  visibleEnd = 2,
+) {
+  if (text.length <= visibleStart) {
+    return text;
+  }
+  return (
+    text.slice(0, visibleStart) +
+    '*'.repeat(hiddenLen) +
+    text.slice(-visibleEnd)
+  );
+}
+
+const ApiKeyDisplay: React.FC<ApiKeyProps> = ({ text }) => {
+  const transformedText = obfuscateText(text);
+
+  return <div className="key-text text">{transformedText}</div>;
+};
+
+const ValidationStatus: React.FC<StatusProps> = ({ status }) => {
+  switch (status) {
+    case StatusStates.Valid:
+      return (
+        <div className="status-box happy-color text">
+          <CelebrationIcon />
+          <span>Valid!</span>
+        </div>
+      );
+    case StatusStates.Invalid:
+      return (
+        <div className="status-box sad-color text">
+          <ErrorIcon />
+          <span>Not valid</span>
+        </div>
+      );
+    case StatusStates.Loading:
+      return (
+        <div className="status-box text">
+          <HourglassTopIcon />
+          <span>Checking...</span>
+        </div>
+      );
+    default:
+      return <div className="status-box" />;
+  }
 };
 
 const Secrets: React.FC = () => {
   const { executeFunction } = useSquid();
 
-  const [generatedKey, setGeneratedKey] = useState<string>('');
-  const [statusText, setStatusText] = useState<string>('');
+  const [generatedKey, setGeneratedKey] = useState<string>('N/A');
+  const [status, setStatus] = useState<StatusStates>(StatusStates.Idle);
   const [formData, setFormData] = useState({ apiKey: '' });
 
   const generateKey = async (e: React.FormEvent) => {
     e.preventDefault();
-    setGeneratedKey('⏳');
     executeFunction('generateApiKey', 'SOME_KEY_NAME').then((key) => {
       setGeneratedKey(key);
     });
@@ -27,13 +87,13 @@ const Secrets: React.FC = () => {
 
   const checkKey = async (e: React.FormEvent) => {
     e.preventDefault();
-    setStatusText('⏳');
+    setStatus(StatusStates.Loading);
     executeFunction('validateApiKey', 'SOME_KEY_NAME', formData.apiKey).then(
       (confirmed) => {
         if (confirmed) {
-          setStatusText('👍');
+          setStatus(StatusStates.Valid);
         } else {
-          setStatusText('🙅');
+          setStatus(StatusStates.Invalid);
         }
       },
     );
@@ -47,27 +107,52 @@ const Secrets: React.FC = () => {
     }));
   };
 
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  };
+
   return (
     <div>
       <h2>API keys</h2>
 
-      <h3>Generate yourself a key</h3>
-      <form id="generate" onSubmit={generateKey}>
-        <StatusComponent text={generatedKey} />
-        <Button type="submit">Generate New Key</Button>
-      </form>
+      <div className="section">
+        <h3>Your generated key</h3>
+        <div className="side-by-side justify-left key-container">
+          <ApiKeyDisplay text={generatedKey} />
+          <IconButton
+            onClick={() => copyToClipboard(generatedKey)}
+            aria-label="copy"
+            className="key-action"
+          >
+            <ContentCopyIcon />
+          </IconButton>
+        </div>
+      </div>
 
-      <h3>Check if your key is valid</h3>
-      <form id="form" onSubmit={checkKey}>
-        <input
-          name="apiKey"
-          value={formData.apiKey}
-          onChange={handleInputChange}
-          placeholder="Your API key"
-        />
-        <StatusComponent text={statusText} />
-        <Button type="submit">Check Key</Button>
-      </form>
+      <div className="section">
+        <h3>Generate yourself a key</h3>
+        <form id="generate" onSubmit={generateKey}>
+          <button type="submit">Generate New Key</button>
+        </form>
+      </div>
+
+      <div className="section">
+        <h3>Check if your key is valid</h3>
+        <form id="form" onSubmit={checkKey}>
+          <input
+            name="apiKey"
+            value={formData.apiKey}
+            onChange={handleInputChange}
+            placeholder="Enter API key"
+          />
+          <ValidationStatus status={status} />
+          <button type="submit">Check Key</button>
+        </form>
+      </div>
     </div>
   );
 };
--- executables/frontend/src/components/sendEmail.tsx
+++ executables/frontend/src/components/sendEmail.tsx
@@ -1,34 +1,70 @@
 import { useSquid } from '@squidcloud/react';
-import { Button } from '@mui/material';
 import React, { useState } from 'react';
+import CelebrationIcon from '@mui/icons-material/Celebration';
+import ErrorIcon from '@mui/icons-material/Error';
+import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 
-interface StatusProps {
-  text: string;
+enum StatusStates {
+  Idle = 'IDLE',
+  Sending = 'SENDING',
+  Sent = 'SENT',
+  Error = 'ERROR',
 }
 
-const StatusComponent: React.FC<StatusProps> = ({ text }) => {
-  return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );
+interface StatusProps {
+  status: StatusStates;
+  message?: string;
+}
+
+const StatusComponent: React.FC<StatusProps> = ({ status, message }) => {
+  switch (status) {
+    case StatusStates.Sent:
+      return (
+        <div className="status-box happy-color text">
+          <CelebrationIcon />
+          <span>Sent!</span>
+        </div>
+      );
+    case StatusStates.Error:
+      return (
+        <div className="status-box sad-color text">
+          <ErrorIcon />
+          <span>{message}</span>
+        </div>
+      );
+    case StatusStates.Sending:
+      return (
+        <div className="status-box text">
+          <HourglassTopIcon />
+          <span>Checking...</span>
+        </div>
+      );
+    default:
+      return <div className="status-box" />;
+  }
 };
 
 const SendEmail: React.FC = () => {
   const { executeFunction } = useSquid();
 
   const [formData, setFormData] = useState({ address: '', body: '' });
-  const [emailStatus, setEmailStatus] = useState<string>('');
+  const [emailStatus, setEmailStatus] = useState<{
+    status: StatusStates;
+    message?: string;
+  }>({ status: StatusStates.Idle });
 
   const sendEmail = (e: React.FormEvent) => {
     e.preventDefault();
     const bodyContent = formData.body;
     const returnAddress = formData.address;
     if (!bodyContent.trim() || !returnAddress.trim()) {
-      setEmailStatus('Both text boxes are required.');
+      setEmailStatus({
+        status: StatusStates.Error,
+        message: 'Both text boxes are required.',
+      });
       return;
     } else {
-      setEmailStatus('Sending...');
+      setEmailStatus({ status: StatusStates.Sending });
     }
     executeFunction(
       'sendEmail',
@@ -37,10 +73,13 @@ const SendEmail: React.FC = () => {
       bodyContent,
     ).then((confirmed) => {
       if (confirmed) {
-        setEmailStatus('Sent!');
+        setEmailStatus({ status: StatusStates.Sent });
         setFormData({ address: '', body: '' });
       } else {
-        setEmailStatus('Failed to send! Is your Nodemailer config correct?');
+        setEmailStatus({
+          status: StatusStates.Error,
+          message: 'Failed to send! Is your Nodemailer config correct?',
+        });
       }
     });
   };
@@ -64,17 +103,19 @@ const SendEmail: React.FC = () => {
           name="address"
           value={formData.address}
           onChange={handleInputChange}
-          placeholder="Your email address"
+          placeholder="Email Address"
         />
-        <br />
         <input
           name="body"
           value={formData.body}
           onChange={handleInputChange}
-          placeholder="Your message here"
+          placeholder="Message"
         />
-        <StatusComponent text={emailStatus} />
-        <Button type="submit">Send</Button>
+        <StatusComponent
+          status={emailStatus.status}
+          message={emailStatus.message}
+        />
+        <button type="submit">Send</button>
       </form>
     </div>
   );
