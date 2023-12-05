774502b1805e8acc1fbccba1b4f3e6c471d14fa6
Jen Person <jen@squid.cloud>
2023-12-05 13:12:54 -0600

    saving changes for feedback on PR

--- react-crud/react-crud-frontend/src/components/UsersTable.tsx
+++ react-crud/react-crud-frontend/src/components/UsersTable.tsx
@@ -6,8 +6,8 @@ export default function UsersTable() {
     <table>
       <thead>
         <tr>
-          <th style={{ fontSize: '24px' }}>Crew Members</th>
-          <th>
+          <th style={{ fontSize: '1.2em' }}>Crew Members</th>
+          <th colSpan={2}>
             <CreateUser />
           </th>
         </tr>
