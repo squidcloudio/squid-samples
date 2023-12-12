63113cb96faa30926da01b1c8934d26853018912
Jen Person <jen@squid.cloud>
2023-12-12 17:03:52 -0600

    removing unneeded logs

--- ai-for-data/ai-for-data-backend/src/service/data-ai-service.ts
+++ ai-for-data/ai-for-data-backend/src/service/data-ai-service.ts
@@ -11,11 +11,6 @@ export class SquidAiDataService extends SquidService {
   @executable()
   async askQuestion(question: string): Promise<AiResponse> {
     const aiResponse = await this.squid.ai().executeAiQuery('built_in_db', question);
-    console.log(
-      `Question: ${question} 
-        Query: ${aiResponse.executedQuery ?? 'No query executed'} 
-        Explanation: ${aiResponse.explanation ?? 'No explanation'}`,
-    );
     return {
       author: 'ai',
       answer: aiResponse.answer,
