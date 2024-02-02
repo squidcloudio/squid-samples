e6f69a6afa979bcee97f8050b04363217d754437
Victor Chang <victor@squid.cloud>
2024-02-02 14:09:47 -0800

    More correct fix.

--- angular-crud/angular-crud-frontend/src/app/read-users/read-users.component.ts
+++ angular-crud/angular-crud-frontend/src/app/read-users/read-users.component.ts
@@ -6,26 +6,18 @@ type User = { id: string; email: string; age: number };
 @Component({
   selector: 'read-users',
   template: `
-    <table>
-      <thead>
-        <tr>
-          <th>Email</th>
-          <th>Age</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let user of users | async">
-          <td>
-            {{ user.email }}
-          </td>
-          <td>
-            {{ user.age }}
-          </td>
-          <!-- options contains update and delete -->
-          <options [id]="user.id" />
-        </tr>
-      </tbody>
-    </table>
+    <tbody>
+      <tr *ngFor="let user of users | async">
+        <td>
+          {{ user.email }}
+        </td>
+        <td>
+          {{ user.age }}
+        </td>
+        <!-- options contains update and delete -->
+        <options [id]="user.id" />
+      </tr>
+    </tbody>
   `,
 })
 export class ReadUsersComponent {
